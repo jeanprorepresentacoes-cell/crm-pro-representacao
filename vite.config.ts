db1797ce3e38d637f4c0878a9eb3a5d32d62b08f
bazartquadros <bazartquadros@gmail.com>
2025-11-28 14:10:34 -0300

    Fix: Adiciona configuração de PostCSS para carregar estilos

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  // Diz à Vite que a raiz do nosso projeto frontend é a pasta 'client'
   root: 'client', 
   
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+
   plugins: [react()],
   
   resolve: {
     alias: {
-      // Agora que a raiz é 'client', o atalho '@' aponta para 'client/src'
       '@': path.resolve(__dirname, './client/src'),
-      // E o atalho '@shared' aponta para a pasta 'shared' que está um nível acima
       '@shared': path.resolve(__dirname, './shared'),
     },
   },
 
   build: {
-    // Diz à Vite para colocar o resultado final na pasta 'dist' na raiz do projeto
     outDir: '../dist',
   },
 
